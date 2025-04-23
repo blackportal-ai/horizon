eaa4cc2d3f16c7de4971126746c3d4c7adecb965
Isaac Dobbins <isaac.dobbins@icloud.com>
2025-04-23 16:44:12 -0600

    Exploring what type-state would look like for GPU resources. Also fixing some protobuf compiler errors related to it not existing by default. Rust now manages the installation of protoc.

--- crates/rpc/build.rs
+++ crates/rpc/build.rs
@@ -1,8 +1,7 @@
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let protoc_path = protoc_bin_vendored::protoc_bin_path()?;
-    unsafe {std::env::set_var("PROTOC", protoc_path) };
-    
+    unsafe { std::env::set_var("PROTOC", protoc_path) };
+
     tonic_build::compile_protos("proto/client.proto")?;
     tonic_build::compile_protos("proto/marketplace.proto")?;
     Ok(())
--- crates/types/src/lib.rs
+++ crates/types/src/lib.rs
@@ -1,14 +1,97 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::marker::PhantomData;
+
+struct Idle;
+struct Allocated;
+struct Preparing;
+struct Computing;
+struct Finalizing;
+struct Suspended;
+struct Faulty;
+
+struct GPU<State> {
+    id: u32,
+    _phantom: PhantomData<State>,
 }
+
+impl GPU<Idle> {
+    fn allocate(self, _task: Task) -> GPU<Allocated> {
+        GPU {
+            id: self.id,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl GPU<Allocated> {
+    fn start_preparing(self) -> GPU<Preparing> {
+        GPU {
+            id: self.id,
+            _phantom: PhantomData,
+        }
+    }
+    fn deallocate(self) -> GPU<Idle> {
+        GPU {
+            id: self.id,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl GPU<Preparing> {
+    fn start_computing(self) -> GPU<Computing> {
+        GPU {
+            id: self.id,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl GPU<Computing> {
+    fn suspend(self) -> GPU<Suspended> {
+        GPU {
+            id: self.id,
+            _phantom: PhantomData,
+        }
+    }
+    fn finish_computing(self) -> GPU<Finalizing> {
+        GPU {
+            id: self.id,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl GPU<Suspended> {
+    fn resume(self) -> GPU<Computing> {
+        GPU {
+            id: self.id,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl GPU<Finalizing> {
+    fn complete(self) -> GPU<Idle> {
+        GPU {
+            id: self.id,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl GPU<Faulty> {
+    fn repair(self) -> GPU<Idle> {
+        GPU {
+            id: self.id,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+// Dummy Task type
+struct Task;
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
