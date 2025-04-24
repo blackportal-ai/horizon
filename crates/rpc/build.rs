10ad15530801724ee5847991f997bfdd922c5880
Isaac Dobbins <isaac.dobbins@icloud.com>
2025-04-24 17:12:57 -0600

    working on .proto file

--- crates/rpc/build.rs
+++ crates/rpc/build.rs
@@ -2,7 +2,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let protoc_path = protoc_bin_vendored::protoc_bin_path()?;
     unsafe { std::env::set_var("PROTOC", protoc_path) };
 
-    tonic_build::compile_protos("proto/client.proto")?;
-    tonic_build::compile_protos("proto/marketplace.proto")?;
+    tonic_build::compile_protos("proto/horizon.proto")?;
     Ok(())
 }
